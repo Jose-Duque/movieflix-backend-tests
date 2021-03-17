6f0a648caf18254fc3a0912705a6d984d6c8ac35
José Carlos Duque Filho <cj_duque@hotmail.com>
2021-03-17 16:01:42 -0300

    Add User FindById

--- backend/src/main/java/com/devsuperior/movieflix/services/UserService.java
+++ backend/src/main/java/com/devsuperior/movieflix/services/UserService.java
@@ -1,6 +1,7 @@
 package com.devsuperior.movieflix.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.movieflix.dto.UserDTO;
 import com.devsuperior.movieflix.entities.User;
 import com.devsuperior.movieflix.repositories.UserRepository;
+import com.devsuperior.movieflix.services.exceptions.EntityNotFoundException;
 
 @Service
 public class UserService {
@@ -21,5 +23,12 @@ public class UserService {
 	public List<UserDTO> findAll() {
 		List<User> users = repository.findAll();
 		return users.stream().map(user -> new UserDTO(user)).collect(Collectors.toList());
+	}
+	
+	@Transactional(readOnly = true)
+	public UserDTO finById(Long id) {
+		Optional<User> user = repository.findById(id);
+		User obj = user.orElseThrow(() -> new EntityNotFoundException("Usuário não encontrado"));
+		return new UserDTO(obj);
 	}
 }
