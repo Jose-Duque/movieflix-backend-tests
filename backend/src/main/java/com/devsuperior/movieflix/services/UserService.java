d4a8de65479837c3cf0e15a3b22f10535b5f6738
José Carlos Duque Filho <cj_duque@hotmail.com>
2021-03-22 13:57:29 -0300

    Implementing oauth2 authorization server

--- backend/src/main/java/com/devsuperior/movieflix/services/UserService.java
+++ backend/src/main/java/com/devsuperior/movieflix/services/UserService.java
@@ -4,8 +4,11 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,10 +18,9 @@ import com.devsuperior.movieflix.repositories.UserRepository;
 import com.devsuperior.movieflix.services.exceptions.ResourceNotFoundException;
 
 @Service
-public class UserService {
-
-	@Autowired
-	private BCryptPasswordEncoder passwordEncoder;
+public class UserService implements UserDetailsService {
+	
+	private static Logger logger = org.slf4j.LoggerFactory.getLogger(UserService.class);
 	
 	@Autowired
 	private UserRepository repository;
@@ -34,5 +36,16 @@ public class UserService {
 		Optional<User> user = repository.findById(id);
 		User obj = user.orElseThrow(() -> new ResourceNotFoundException("Usuário não encontrado"));
 		return new UserDTO(obj);
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = repository.findByEmail(username);
+		if(user == null) {
+			logger.error("E-mail not found " + username);
+			throw new UsernameNotFoundException("E-mail not found" + username);
+		}
+		logger.info("User found " + username);
+		return user;
 	}
 }
